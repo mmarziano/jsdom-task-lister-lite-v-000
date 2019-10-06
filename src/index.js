c83d06c8b143748dca457e7f33953b7f250a179f
Monica Marziano <mmarziano1@gmail.com>
2019-10-06 12:41:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let tasks = document.querySelector('#tasks');
+  let form = document.querySelector('#create-task-form');
+  
+  form.addEventListener('submit', function(e){
+     e.preventDefault();
+    let input = form.elements[0].value;
+    let priority = form.elements[1].value;
+    tasks.append(input)
+    tasks.append(priority)
+  
+     
+  });
+  
 });
