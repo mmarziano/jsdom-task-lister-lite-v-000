ace2090657a1b3107bcebd2f6631d43eace2886c
Monica Marziano <mmarziano1@gmail.com>
2019-10-06 12:21:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let tasks = document.getElementById('#tasks');
+  let form = document.querySelector('#create-task-form');
+  
+  form.addEventListener('submit', function(e){
+     e.preventDefault();
+     let input = form.elements[0].val;
+     console.log(input)
+     tasks.append('<li>' + input + '</li>')
+     
+  });
+  
 });
